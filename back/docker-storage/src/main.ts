60480792926eb1d59789fbc0c34c01ef27d3871d
marobert <marobert@student.42lyon.fr>
2023-08-30 18:36:57 +0200

    add swagger to see all api routes

--- back/docker-storage/src/main.ts
+++ back/docker-storage/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Transcendence')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
