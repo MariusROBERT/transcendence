d425a8edef3633aaef243c2c4979c95ad9457401
marobert <marobert@student.42lyon.fr>
2023-08-31 10:38:17 +0200

    improved startup scripts

--- back/script.sh
+++ back/script.sh
@@ -1,3 +1,9 @@
+if [ ! -d /app ]
+then
+  nest new /app --package-manager yarn
+  rm -rf /app/.git
+fi
+
 yarn install
 yarn upgrade
 yarn start:dev
--- front/script.sh
+++ front/script.sh
@@ -1,3 +1,8 @@
+if [ ! -d /app ]
+then
+  npx create-react-app /app --template typescript
+fi
+
 npm install
 npm update
 npm start
