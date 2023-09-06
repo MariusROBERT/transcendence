4f675f6445fffafa9fba72c1ffb8af2b7cbc40fe
Guillaume Simonet <guillaumemnmsimonet@gmail.com>
2023-09-05 18:58:00 -0700

    dep add in script for docker

--- back/script.sh
+++ back/script.sh
@@ -6,4 +6,13 @@ fi
 
 yarn install
 yarn upgrade
+
+
+# Accédez au répertoire /app
+cd /app
+
+# Installation des dépendances spécifiées
+yarn add @nestjs/typeorm typeorm mysql2 class-validator class-transformer @nestjs/websockets @nestjs/platform-socket.io bcrypt
+yarn add --dev @types/bcrypt @nestjs/passport passport @nestjs/jwt passport-jwt
+
 yarn start
