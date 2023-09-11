f30c1b02b2a99c5ebc189e984ac16d0bd6f24f05
Guillaume Simonet <guillaumemnmsimonet@gmail.com>
2023-09-11 11:27:49 -0700

    b4push

--- back/docker-storage/src/database/database.module.ts
+++ back/docker-storage/src/database/database.module.ts
@@ -1,23 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ChannelEntity, MessageEntity, UserEntity } from "./entities/channel.entity";
-import {  } from "./entities/user.entity";
 import { ChannelService } from "src/channel/channel.service";
 import { UserService } from "src/user/user.service";
 import { MessagesService } from "src/messages/messages.service";
 import { AuthService } from "src/auth/auth.service";
 import { JwtService } from "@nestjs/jwt";
 
-// pour lancer avec docker :
-//  deja : npm install --save @nestjs/config
-//  creer un role pour la db (user)
-//  creer db
-
 @Module({
     imports: [
-        // ConfigModule.forRoot({
-        //     isGlobal: true // module vue par tous les modules
-        //   }),
         TypeOrmModule.forRoot({ // config DB mysql
             type: 'postgres',
             host: process.env.DB_HOST,
