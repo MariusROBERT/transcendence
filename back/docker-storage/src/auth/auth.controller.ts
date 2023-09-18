c4238d544e29c37b8ee48e38e0adcc4e26a3c86e
marobert <marobert@student.42lyon.fr>
2023-09-18 19:42:19 +0200

    login with 42 create a user or login to it, in both case, return a JWT token

--- back/docker-storage/src/auth/auth.controller.ts
+++ back/docker-storage/src/auth/auth.controller.ts
@@ -11,10 +11,12 @@ import { UserSubDto } from './dtos/user-sub.dto';
 import { UserEntity } from '../database/entities/user.entity';
 import { LoginCreditDto } from './dtos/login-credit.dto';
 import { FtOAuthGuard } from './guards/ft-auth.guards';
+import { ftLoginDto } from './dtos/ft-login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+  }
 
   @Post('/register')
   async Register(@Body() userData: UserSubDto): Promise<Partial<UserEntity>> {
@@ -40,7 +42,7 @@ export class AuthController {
   @Get('callback/42')
   @UseGuards(FtOAuthGuard)
   auth42callback(@Request() req) {
-    return { username: req.user.username, image: req.user?._json.image.link };
-    // return req.user;
+    // const user : ftLoginDto = { username: req.user.username, urlImg: req.user._json.image_url };
+    return this.authService.ftLogin({ username: req.user.username, urlImg: req.user._json.image_url } as ftLoginDto);
   }
 }
--- back/docker-storage/src/auth/strategy/passport-ft.strategy.ts
+++ back/docker-storage/src/auth/strategy/passport-ft.strategy.ts
@@ -18,12 +18,12 @@ export class FtStrategy extends PassportStrategy(Strategy, '42') {
     accessToken: string,
     refreshToken: string,
     profile: Profile,
-  ): Promise<boolean> {
+  ): Promise<any> {
     request.session.accessToken = accessToken;
-    console.log('accessToken: ', accessToken);
-    console.log('refreshToken: ', refreshToken);
-    console.log('profile: ', profile);
-    console.log('login: ', profile.username);
+    // console.log('accessToken: ', accessToken);
+    // console.log('refreshToken: ', refreshToken);
+    // console.log('profile: ', profile);
+    // console.log('login: ', profile.username);
     return profile;
     // return profile?.provider === '42';
   }
