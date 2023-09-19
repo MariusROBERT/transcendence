6e6e5dc0772a71135910329d6f80ff7a701965de
Raphael Bonneval <grugroo128@gmail.com>
2023-09-19 19:31:06 +0200

    Update User.tsx

--- front/docker-storage/src/components/User/User.tsx
+++ front/docker-storage/src/components/User/User.tsx
@@ -37,7 +37,7 @@ export function User({icon_url = require('../../assets/imgs/icon_user.png'), use
     return (
         <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between', borderRadius:'12.5px', backgroundColor:color.grey, minWidth:'410px', height:'25px'}}>
             <Flex zIndex={'10'} flex_direction="row">
-                <RoundButton icon={icon_url} icon_size={50} onClick={openProfile}></RoundButton>
+                <RoundButton icon={icon_url ? icon_url : require('../../assets/imgs/icon_user.png')} icon_size={50} onClick={openProfile}></RoundButton>
                 <p>{user_name}</p>
             </Flex>
             <Flex zIndex={'10'} flex_direction="row" flex_justifyContent={'space-evenly'}>
