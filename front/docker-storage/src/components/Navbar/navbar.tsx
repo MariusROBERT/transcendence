fceedeaec7d2fcf7ad7f50a316e8e395db874e47
marobert <mariusrobert380@gmail.com>
2023-09-27 19:49:48 +0200

    Remove JWT cookie when disconnect

--- front/docker-storage/src/components/Navbar/navbar.tsx
+++ front/docker-storage/src/components/Navbar/navbar.tsx
@@ -6,13 +6,13 @@ import Profil from '../Profil/profil';
 import Cookies from 'js-cookie';
 import { useNavigate } from 'react-router-dom';
 
-interface Props{
-  meUser: IUserComplete|undefined
+interface Props {
+  meUser: IUserComplete | undefined;
 }
 
-const Navbar: React.FC<Props>  = ({ meUser }) => {
+const Navbar: React.FC<Props> = ({ meUser }) => {
   const jwtToken = Cookies.get('jwtToken');
-	const navigate = useNavigate();
+  const navigate = useNavigate();
   const [settingsVisible, setSettingsVisible] = useState<boolean>(false);
   const [profilVisible, setProfilVisible] = useState<boolean>(false);
 // todo : mettre le Fetch
@@ -24,19 +24,24 @@ const Navbar: React.FC<Props>  = ({ meUser }) => {
         Authorization: `Bearer ${jwtToken}`,
       },
     })
-    .then(() => {
+      .then(() => {
+        Cookies.remove('jwtToken');
         navigate('/login');
-    });
-  }
+      });
+  };
 
   return (
     <>
       <div style={navbarStyle}>
-          <RoundButton icon={require('../../assets/imgs/icon_setting.png')} icon_size={50} onClick={() => setSettingsVisible(!settingsVisible)}></RoundButton>
-          {settingsVisible && <Settings isVisible={settingsVisible} />}
-          <RoundButton icon={require('../../assets/imgs/icon_user.png')} icon_size={50} onClick={() => setProfilVisible(!profilVisible)}></RoundButton>
-          {profilVisible && <Profil otherUser={meUser} meUser={meUser} onClose={() => setProfilVisible(!profilVisible)}/>}
-          <RoundButton icon={require('../../assets/imgs/icon_logout.png')} icon_size={50} onClick={() => logout()}></RoundButton>
+        <RoundButton icon={require('../../assets/imgs/icon_setting.png')} icon_size={50}
+                     onClick={() => setSettingsVisible(!settingsVisible)}></RoundButton>
+        {settingsVisible && <Settings isVisible={settingsVisible} />}
+        <RoundButton icon={require('../../assets/imgs/icon_user.png')} icon_size={50}
+                     onClick={() => setProfilVisible(!profilVisible)}></RoundButton>
+        {profilVisible &&
+          <Profil otherUser={meUser} meUser={meUser} onClose={() => setProfilVisible(!profilVisible)} />}
+        <RoundButton icon={require('../../assets/imgs/icon_logout.png')} icon_size={50}
+                     onClick={() => logout()}></RoundButton>
       </div>
     </>
   );
@@ -44,7 +49,7 @@ const Navbar: React.FC<Props>  = ({ meUser }) => {
 
 const navbarStyle = {
   border: '1px solid black',
-  display: 'flex'
+  display: 'flex',
 };
 
 
