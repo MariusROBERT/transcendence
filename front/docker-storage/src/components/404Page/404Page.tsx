1661b54d1dcd7964bc0ea5a0b08a7bc351b7230f
marobert <mariusrobert380@gmail.com>
2023-09-28 12:34:47 +0200

    Improved style

--- front/docker-storage/src/components/404Page/404Page.tsx
+++ front/docker-storage/src/components/404Page/404Page.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <a href="/">Retour a la page principale</a>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
+      <p style={{ fontSize: '7em' }}>404 - Page Not Found</p>
+      <p>
+        <a href='/' style={{color:'lightgrey', fontSize:'1.5em'}}>Retour a la page principale</a>
+      </p>
     </div>
   );
 };
