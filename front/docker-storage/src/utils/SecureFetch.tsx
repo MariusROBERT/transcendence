4c122e8c0aeb0593a0537ac4c0d6ac5edec3ac14
villam27 <villam2703@protonmail.com>
2023-09-29 18:10:57 +0200

    [Rem] The test fetch

--- front/docker-storage/src/utils/SecureFetch.tsx
+++ front/docker-storage/src/utils/SecureFetch.tsx
@@ -16,15 +16,40 @@ export async function Fetch(url_end: string, method: 'GET' | 'PATCH' | 'POST', b
       return { response: response, json: rep_json };
     } else {
       console.error('You have been disconnected \n(your Authorisation Cookie has been modified or deleted)');
-      //window.location.href = '/login';
-      console.log(await response.json());
+      window.location.href = '/login';
     }
   } catch (e) {
     console.log(e);
     console.error('You have been disconnected \n(your Authorisation Cookie has been modified or deleted)');
-    //window.location.href = '/login';  TODO: remove before pull request its only for test
+    window.location.href = '/login';
   }
 }
+
+//export async function Fetch(url_end: string, method: 'GET' | 'PATCH' | 'POST', body: any = undefined): Promise<undefined | { response: Response, json: any }> {
+//  const jwtToken = Cookies.get('jwtToken');
+//  try {
+//    const response = await fetch('http://localhost:3001/api/' + url_end, {
+//      method: method,
+//      headers: {
+//        'Content-Type': 'application/json',
+//        Authorization: `Bearer ${jwtToken}`,
+//      },
+//      body: body
+//    })
+//    if (response.ok) {
+//      const rep_json = await response.json();
+//      return { response: response, json: rep_json };
+//    } else {
+//      console.error('You have been disconnected \n(your Authorisation Cookie has been modified or deleted)');
+//      //window.location.href = '/login';
+//      console.log(await response.json());
+//    }
+//  } catch (e) {
+//    console.log(e);
+//    console.error('You have been disconnected \n(your Authorisation Cookie has been modified or deleted)');
+//    //window.location.href = '/login';  TODO: remove before pull request its only for test
+//  }
+//}
 
 export async function unsecureFetch(
   url_end: string,
