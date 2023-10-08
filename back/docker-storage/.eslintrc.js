f702aa4dcd509900ae1064688275a4e332b7d47f
marobert <mariusrobert380@gmail.com>
2023-10-08 18:51:29 +0200

    Update ESLint rules

--- back/docker-storage/.eslintrc.js
+++ back/docker-storage/.eslintrc.js
@@ -24,5 +24,14 @@ module.exports = {
     'no-var': 'error',
     quotes: ['error', 'single', { avoidEscape: true }],
     'prefer-const': 'error',
+    'no-else-return': ['error', {allowElseIf: false}],
+    'no-unneeded-ternary': 'error',
+    'no-restricted-syntax': [
+      'error',
+      {
+        'selector': 'BinaryExpression[operator=/^(==|===|!=|!==)$/][left.raw=/^(true|false)$/], BinaryExpression[operator=/^(==|===|!=|!==)$/][right.raw=/^(true|false)$/]',
+        'message': 'Don\'t compare for equality against boolean literals'
+      }
+    ]
   },
 };
--- front/docker-storage/.eslintrc.js
+++ front/docker-storage/.eslintrc.js
@@ -14,15 +14,24 @@ module.exports = {
         node: true,
         jest: true,
     },
-    ignorePatterns: ['.eslintrc.js.old'],
+    ignorePatterns: ['.eslintrc.js'],
     rules: {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         'no-var': 'error',
-        quotes: ['error', 'single', { avoidEscape: true }],
+        quotes: ['error', 'single', {avoidEscape: true}],
         'prefer-const': 'error',
         '@typescript-eslint/no-var-requires': 'off',
+        'no-else-return': ['error', {allowElseIf: false}],
+        'no-unneeded-ternary': 'error',
+        'no-restricted-syntax': [
+            'error',
+            {
+                'selector': 'BinaryExpression[operator=/^(==|===|!=|!==)$/][left.raw=/^(true|false)$/], BinaryExpression[operator=/^(==|===|!=|!==)$/][right.raw=/^(true|false)$/]',
+                'message': 'Don\'t compare for equality against boolean literals'
+            }
+        ]
     },
 };
