7d44fbb28f43105184d022b93369849993f765ae
marobert <mariusrobert380@gmail.com>
2023-10-16 13:23:20 +0200

    Fixed merge

--- front/docker-storage/src/App.tsx
+++ front/docker-storage/src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import { color, useEffectViewport, Viewport } from './utils';
-import { AuthGuard, Game, GameScore, Login, MainPage, NotFoundPage } from './components';
-import { backgroundImage, color, useEffectViewport, Viewport } from './utils';
-import { AuthGuard, Background, Game, GameScore, Login, MainPage, NotFoundPage } from './components';
-import { Route, Routes } from 'react-router-dom';
-import { Contexts } from './contexts';
-import { AnimatedBackground } from './components/ComponentBase/AnimatedBackground';
+import React, {useState} from 'react';
+import {color, useEffectViewport, Viewport} from './utils';
+import {AuthGuard, Game, GameScore, Login, MainPage, NotFoundPage} from './components';
+import {Route, Routes} from 'react-router-dom';
+import {Contexts} from './contexts';
+import {AnimatedBackground} from './components/ComponentBase/AnimatedBackground';
 
 
 function App() {
@@ -35,27 +33,27 @@ function App() {
         className={'cursor_perso'}
         style={appStyle}
       >
-        <AnimatedBackground viewport={viewport} ballNumber={10} style={{zIndex:'-1'}}/>
+        <AnimatedBackground viewport={viewport} ballNumber={10} style={{zIndex: '-1'}}/>
         <Routes>
-          <Route path='/login' element={
+          <Route path="/login" element={
             <Login viewport={viewport}></Login>}>
           </Route>
-          <Route path='/' element={
+          <Route path="/" element={
             <AuthGuard isAuthenticated>
               <MainPage panelWidth={SIZE} viewport={viewport}></MainPage>
             </AuthGuard>}>
           </Route>
-          <Route path='/game' element={
+          <Route path="/game" element={
             <AuthGuard isAuthenticated>
               <Game viewport={viewport}></Game>
             </AuthGuard>}>
           </Route>
-          <Route path='/game/score' element={
+          <Route path="/game/score" element={
             <AuthGuard isAuthenticated>
               <GameScore viewport={viewport}></GameScore>
             </AuthGuard>}>
           </Route>
-          <Route path='*' element={<NotFoundPage />} />
+          <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
       </div>
     </Contexts>
