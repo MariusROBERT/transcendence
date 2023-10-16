1f0b873fe40b20f531ccfddeed16dd36649c1200
Johan Litaudon <jlitaudo@z3r6p3.42lyon.fr>
2023-10-16 11:36:37 +0200

    modif on theWinnerIs

--- front/docker-storage/src/components/Game/TheWinnerIs.tsx
+++ front/docker-storage/src/components/Game/TheWinnerIs.tsx
@@ -3,6 +3,5 @@
 export function TheWinnerIs({ won }: { won:boolean }){
     if (won)
       return (<h4>YOU WON!!</h4>);
-    else
-      return (<h4>YOU LOSE!!</h4>)
+    return (<h4>YOU LOSE!!</h4>);
   }
