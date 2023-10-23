15683b5af13779b62012627ef540ec386df1296d
Johan Litaudon <johan.litaudon@gmail.com>
2023-10-23 17:58:15 +0200

    modification of history banner and profil pannel

---
+++ front/docker-storage/src/components/Profil/GameHistoryBanner.tsx
@@ -0,0 +1,64 @@
+import { GameHistory, IUser } from '../../utils/interfaces';
+import { AuthGuard, Flex, RoundButton, Profil, Popup } from '..';
+import React, { CSSProperties, useState } from 'react';
+
+interface Props {
+    game:GameHistory;
+}
+
+const GameHistoryBanner = ({game }: Props) => {
+    return (
+        <div style={{marginBottom:'5px', alignItems:'column', justifyContent: 'center', display: 'flex', position:'relative'}}>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            margin: '4px 0',
+            padding: '0 4px',
+            background:game.scoreUser>game.scoreOpponent? 'radial-gradient(circle, rgba(9,6,64,1) 0%, rgba(0,212,255,1) 100%)':
+            'radial-gradient(circle, rgba(62,6,64,1) 14%, rgba(255,4,108,1) 100%)',
+            borderRadius: '30px',
+          }}>
+            <Flex flex_direction="column">
+              <div style={nameStyle}>
+                  {game.user}
+              </div>
+              <div>
+                <RoundButton icon={game.urlImgUser} icon_size={60}
+                             onClick={() => null}/>
+              </div>
+            </Flex>
+            <Flex flex_alignItems="center">
+              <p style={{marginRight:'20px', marginLeft:'15px'}}>{game.eloUser}</p>
+            </Flex>
+            <Flex flex_alignItems="center">
+              <p style={{marginRight:'20px', fontSize:'35px'}}>{game.scoreUser === -1 ? 'FF' : game.scoreUser} - {game.scoreOpponent=== -1 ? 'FF' : game.scoreOpponent}</p>
+            </Flex>
+            <Flex flex_alignItems="center">
+              <p style={{marginRight:'15px'}}>{game.eloOpponent}</p>
+            </Flex>
+            <Flex flex_direction="column">
+              <div style={nameStyle}>
+                  {game.opponent}
+              </div>
+              <div>
+                <RoundButton icon={game.urlImgOpponent} icon_size={60}
+                             onClick={() => null}/>
+              </div>
+            </Flex>
+          </div>
+          <p style={{marginRight:'20px', fontSize:'8px', position:'absolute', bottom:'0'}}>{game.date.toLocaleString().slice(0,10)}</p>
+        </div>
+      );
+    }
+    
+    const nameStyle: CSSProperties = {
+      marginTop: '5px',
+      fontWeight: 'bold',
+      textShadow: '2px 2px 4px #000000',
+      fontSize: 20,
+      minWidth: '11ch',
+      textAlign:'center',
+      fontFamily: 'robot_font, sans-serif'
+    };
+    
+    export default GameHistoryBanner;
--- front/docker-storage/src/components/Profil/Profil.tsx
+++ front/docker-storage/src/components/Profil/Profil.tsx
@@ -2,9 +2,10 @@ import { GameHistory, IUser } from '../../utils/interfaces';
 import React, { useEffect, useState } from 'react';
 import { UserButton } from '../User/UserButton';
 import { useUserContext } from '../../contexts';
-import { Popup } from '../index';
+import { Flex, Popup } from '../index';
 import { useUIContext } from '../../contexts/UIContext/UIContext';
 import { Fetch } from '../../utils';
+import GameHistoryBanner from './GameHistoryBanner';
 
 
 export default function Profil() {
@@ -37,7 +38,7 @@ export default function Profil() {
     alignItems: 'center',
     display: 'flex',
     flexDirection: 'column',
-    background: 'grey',
+    background: '#00375C',
     height: '100%',
     color: 'white',
     margin: mobile ? 5 : 10,
@@ -78,19 +79,65 @@ export default function Profil() {
           <h3>{profilUser?.username}</h3> :
           <h2>{profilUser?.username}</h2>
         }
-        <p>ID : {profilUser?.id}</p>
+        <Flex flex_direction='row'>
+        <div style={{alignItems:'center'}}>
         <img style={imgStyle} src={profilUser?.urlImg} alt={'user'} />
         <img style={isProfileOpen !== id ? statusStyle : (profilUser?.user_status ? statusStyle : imgStyle)}
              src={profilUser?.user_status === 'on' ? require('../../assets/imgs/icon_green_connect.png') :
                require('../../assets/imgs/icon_red_disconnect.png')}
              alt={profilUser?.user_status === 'on' ? 'connected' : 'disconnected'} />
-        <hr style={{ width: '100%' }} />
-        {/* <div style={MatchHistory}>
+        </div>
+          <div style={{marginLeft:'50px'}}>
+            <div style={{flexDirection:'row', display:'flex'}}>
+              <p style={{minWidth:'13ch'}}>Rank </p> <p> : {user?.elo}</p>
+            </div>
+            <div style={{flexDirection:'row', display:'flex'}}>
+              <p style={{minWidth:'13ch'}}>Elo </p><p>: {user?.elo}</p>
+            </div>
+            <div style={{flexDirection:'row', display:'flex'}}>
+              <p style={{minWidth:'13ch'}}>Winrate</p><p> : {user?.winrate} %</p>
+            </div>
+            <div style={{flexDirection:'row', display:'flex'}}>
+              <p style={{minWidth:'13ch'}}>Games played </p><p> : {user?.gamesPlayed}</p>
+            </div>
+          </div>
+        </Flex>
+        <hr style={{ width: '100%' }} /> 
+        <div>
+          <p style={{
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            margin: '10px 0',
+            padding:'1px 0',
+            fontSize:'40px'
+          }}>MATCH HISTORY</p>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            margin: '1px 0',
+          }}>
+            <Flex flex_alignItems="center">
+              <p style={{fontSize:'25px', paddingRight:'30px', paddingLeft:'30px', minWidth: '8ch', textAlign:'center'}}>You</p>
+            </Flex>
+            <Flex flex_alignItems="center">
+              <p style={{fontSize:'25px', paddingRight:'30px'}}>Elo</p>
+            </Flex>
+            <Flex flex_alignItems="center">
+              <p style={{fontSize:'35px', paddingRight:'30px'}}>Score</p>
+            </Flex>
+            <Flex flex_alignItems="center">
+              <p style={{fontSize:'25px', paddingRight:'30px'}}>Elo</p>
+            </Flex>
+            <Flex flex_alignItems="center">
+              <p style={{fontSize:'25px', paddingRight:'30px'}}>Opponent</p>
+            </Flex>
+          </div>
+        </div>
+        <div  style={{maxHeight: '400px',overflowY: 'scroll'}}>
           {games.map((game)=>(
-              <GameHistory user={user?.id} game={game} key={game.id}/>
+              <GameHistoryBanner game={game} key={game.id}/>
           ))}
-        </div> */}
-        <p>Winrate : {profilUser?.winrate}</p>
+        </div>
         {isProfileOpen !== id && profilUser && <UserButton otherUser={profilUser} />}
       </div>
     </Popup>
