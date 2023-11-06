bab8699ee86d4788fb9e9a941c657dc3ad4a25ea
Raphael Bonneval <rbonneva@z3r7p5.42lyon.fr>
2023-11-06 16:35:03 +0100

    crop message

--- front/docker-storage/src/components/Chat/ChatMessage.tsx
+++ front/docker-storage/src/components/Chat/ChatMessage.tsx
@@ -19,13 +19,16 @@ export function ChatMessage({ children, data, last, onClick }: Props) {
     <div style={{
       paddingRight: isMe ? 5 : 20,
       paddingLeft: isMe ? 20 : 5,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: isMe ? 'flex-end' : 'flex-start',
     }}>
       {
         last !== data.sender_id &&
         <div
           style={{
             display: 'flex',
-            flexDirection: isMe ? 'row' : 'row-reverse',
+            flexDirection: isMe ? 'row-reverse' : 'row',
           }}
         >
           <RoundButton
@@ -38,19 +41,20 @@ export function ChatMessage({ children, data, last, onClick }: Props) {
       <div
         style={{
           boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
-          flex: 'auto',
           display: 'flex',
-          flexDirection: 'column',
+          alignItems: isMe ? 'flex-start' : 'flex-end',
           borderRadius: '20px',
           overflow: 'hidden',
           marginTop: '5px',
+          width: 'fit-content',
+          maxWidth: '100%',
         }}
       >
         <Background
           bg_color={isMe ? color.green : color.white2}
           flex_direction={'column'}
           flex_alignItems={'stretch'}
-          flex_justifyContent={'flex-start'}
+          flex_justifyContent={isMe ? 'flex-start' : 'flex-end'}
         >
           <p style={{
             padding: '4px',
